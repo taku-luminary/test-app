a4cfb5882559c1c417fe582c11f8a286998e738b
Takuma <tsecond03@gmail.com>
2025-09-10 18:36:35 +0900

    課題１実装

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          test
+          課題1
         </p>
         <a
           className="App-link"
